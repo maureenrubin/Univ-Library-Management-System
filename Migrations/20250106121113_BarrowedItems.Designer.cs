f7e47c7e66857c2a038ac0d965e4a9742bdb4c0e
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-06 20:13:04 +0800

    Added Barrowed Migrations

--- Migrations/20250106121113_BarrowedItems.Designer.cs
+++ Migrations/20250106121113_BarrowedItems.Designer.cs
@@ -4,6 +4,7 @@ using LibraryManagementSystem.Data_Connectivity.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LibraryManagementSystem.Migrations
 {
     [DbContext(typeof(LMSDbContext))]
-    partial class LMSDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250106121113_BarrowedItems")]
+    partial class BarrowedItems
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250106121113_BarrowedItems.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementSystem.Migrations
+{
+    public partial class BarrowedItems : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Courses_CoursesCourseId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CoursesCourseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CoursesCourseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "ConfirmPassword",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Admins");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Courses",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "BookAvailable",
+                table: "Books",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Admins",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordHash",
+                table: "Admins",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Admins",
+                type: "varbinary(max)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "BarrowedItems",
+                columns: table => new
+                {
+                    BarrowedItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BarrowedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    BarrowedPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValueSql: "decimal(18,2)"),
+                    Quantity = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BarrowedItems", x => x.BarrowedItemId);
+                    table.ForeignKey(
+                        name: "FK_BarrowedItems_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BarrowedItems_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CourseId",
+                table: "Users",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarrowedItems_BookId",
+                table: "BarrowedItems",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarrowedItems_UserId",
+                table: "BarrowedItems",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Courses_CourseId",
+                table: "Users",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Courses_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "BarrowedItems");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "BookAvailable",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Admins");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CourseId",
+                table: "Users",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CoursesCourseId",
+                table: "Users",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Course",
+                table: "Courses",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Admins",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CoursesCourseId",
+                table: "Users",
+                column: "CoursesCourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Courses_CoursesCourseId",
+                table: "Users",
+                column: "CoursesCourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
