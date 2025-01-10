c281c99697d333806887d6bf45bca6ef67d98530
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-10 21:55:34 +0800

    Add Category Entity

---
+++ Domain/Entities/BookCategoryEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Entities
+{
+    public class BookCategoryEntity
+    {
+        public int BCategoryId { get; set; }
+
+        public int BookId { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public BooksEntity Book { get; set; }
+    }
+}
