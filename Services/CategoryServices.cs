fb3b13776428dade5e4480dc0d4fe4a389a9f926
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-13 21:07:27 +0800

    Update CategoryServices.cs

--- Services/CategoryServices.cs
+++ Services/CategoryServices.cs
@@ -12,16 +12,32 @@ namespace LibraryManagementSystem.Services
 {
     public class CategoryServices : ICategoryServices
     {
-        private readonly LMSDbContext dbContext;
+        private readonly DbContextOptions<LMSDbContext> _dbContextOptions;
 
-        public CategoryServices(LMSDbContext dbContext)
+        public CategoryServices(DbContextOptions<LMSDbContext> dbContextOptions)
         {
-            this.dbContext = dbContext;
+            this._dbContextOptions = dbContextOptions;
         }
 
         public async Task <IEnumerable<BookCategoryEntity>> GetAllBookCategoriesAsync()
         {
-            return await dbContext.Categories.ToListAsync();
+            try
+            {
+                using (var dbContextOptions = new LMSDbContext(_dbContextOptions))
+                {
+                    var categoriess = await dbContextOptions.BookCategory
+                        .Where(c => c.CategoryName != null && c.BCategoryId != null)
+                        .ToListAsync();
+
+                    return categoriess;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error retrieving all Categories. {ex.Message}", ex);
+            }
+          
+           
         }
     }
 }
