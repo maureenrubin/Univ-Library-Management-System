ad260084932504bf5ac6777fdceeeb4d32c77278
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-16 22:05:47 +0800

    Add Barrow Form

--- Presentation/AdminForms/BarrowBookForm.cs
+++ Presentation/AdminForms/BarrowBookForm.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using LibraryManagementSystem.Domain.Entities;
+using LibraryManagementSystem.Repositories.Interfaces;
+using LibraryManagementSystem.Services.Contracts;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,42 @@ namespace LibraryManagementSystem.Presentation.AdminForms
 {
     public partial class BarrowBookForm : Form
     {
-        public BarrowBookForm()
+        private readonly BooksEntity booksEntity;
+        private readonly IBookServices bookServices;
+        private readonly ICategoryServices categoryServices;
+
+
+        public BarrowBookForm(IBookServices bookServices,
+                              ICategoryServices categoryServices,
+                              BooksEntity booksEntity)
         {
             InitializeComponent();
+            this.bookServices = bookServices;
+            this.categoryServices = categoryServices;
+            this.booksEntity = booksEntity;
+
+
+            LoadBarrowBookDetails();
+        }
+
+        private void LoadBarrowBookDetails()
+        {
+            try
+            {
+                var allBook = bookServices.GetAllBooksAsync();
+
+                if(allBook == null)
+                {
+                    MessageBox.Show("No book", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Failed to load barrowed book details. {ex.Message}", ex);
+            }
+
         }
 
         private void ExitBTN_Click(object sender, EventArgs e)
