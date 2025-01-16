80e02443769db800dda549950bb30cab59d19ef8
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-16 21:57:55 +0800

    Add Barrow Services

---
+++ Services/BarrowServices.cs
@@ -0,0 +1,14 @@
+﻿using LibraryManagementSystem.Services.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Services
+{
+    public class BarrowServices : IBarrowServices
+    {
+
+    }
+}
