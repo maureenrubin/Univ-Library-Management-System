14104655a9bbbd7d5b9f23e9f30c5f1f1a8a9b79
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-17 21:09:40 +0800

    Add Barrow Dto

---
+++ Domain/DTO/BarrowBookDTO.cs
@@ -0,0 +1,29 @@
+﻿using LibraryManagementSystem.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.DTO
+{
+    public class BarrowBookDTO
+    {
+        public int BarrowedItemId { get; set; }
+        public int BookId { get; set; }
+        public int UserId { get; set; }
+
+
+        public DateTime BarrowedDate { get; set; }
+        public DateTime DueDate { get; set; }
+        public int Quantity { get; set; }
+        public decimal BarrowedPrice { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public int BookStock { get; set; }
+        public decimal BookPrice { get; set; }
+        public byte[]? BooksPicture { get; set; }
+
+
+    }
+}
