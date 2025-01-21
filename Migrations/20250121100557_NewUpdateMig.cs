6ab31262e3d373664d170dcbc600ba9304cc39ba
Maureen Rubin <maureenrubin21@gmail.com>
2025-01-21 22:03:58 +0800

    Add New Migrations

---
+++ Migrations/20250121100557_NewUpdateMig.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementSystem.Migrations
+{
+    public partial class NewUpdateMig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Course",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Books");
+
+       
+            migrationBuilder.CreateTable(
+                name: "BarrowBook",
+                columns: table => new
+                {
+                    BarrowedItemId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BarrowedDate = table.Column<DateTime>(type: "datetime2", nullable: false, defaultValueSql: "GETDATE()"),
+                    DueDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    BarrowedPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false, defaultValue: 0m),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    BookStock = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BarrowBook", x => x.BarrowedItemId);
+                    table.ForeignKey(
+                        name: "FK_BarrowBook_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BarrowBook_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CourseId",
+                table: "Users",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarrowBook_BookId",
+                table: "BarrowBook",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BarrowBook_UserId",
+                table: "BarrowBook",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookCategory_BookId",
+                table: "BookCategory",
+                column: "BookId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookCategory_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                principalTable: "BookCategory",
+                principalColumn: "BCategoryId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Courses_CourseId",
+                table: "Users",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "CourseId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookCategory_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Courses_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropTable(
+                name: "BarrowBook");
+
+            migrationBuilder.DropTable(
+                name: "BookCategory");
+
+            migrationBuilder.DropTable(
+                name: "Courses");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Books_CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Books");
+
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordHash",
+                table: "Admins");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Admins");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Course",
+                table: "Users",
+                type: "nvarchar(200)",
+                maxLength: 200,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Books",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ConfirmPassword",
+                table: "Admins",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Admins",
+                type: "nvarchar(255)",
+                maxLength: 255,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
---
+++ Migrations/20250121132550_FixFKDelete.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryManagementSystem.Migrations
+{
+    public partial class FixFKDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BarrowBook_Books_BookId",
+                table: "BarrowBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BarrowBook_Users_UserId",
+                table: "BarrowBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookCategory_CategoryId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "AdminPicture",
+                table: "Admins",
+                type: "varbinary(max)",
+                nullable: true,
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BarrowBook_Books_BookId",
+                table: "BarrowBook",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BarrowBook_Users_UserId",
+                table: "BarrowBook",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookCategory_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                principalTable: "BookCategory",
+                principalColumn: "BCategoryId",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_BarrowBook_Books_BookId",
+                table: "BarrowBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_BarrowBook_Users_UserId",
+                table: "BarrowBook");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Books_BookCategory_CategoryId",
+                table: "Books");
+
+            migrationBuilder.AlterColumn<byte[]>(
+                name: "AdminPicture",
+                table: "Admins",
+                type: "varbinary(max)",
+                nullable: false,
+                defaultValue: new byte[0],
+                oldClrType: typeof(byte[]),
+                oldType: "varbinary(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BarrowBook_Books_BookId",
+                table: "BarrowBook",
+                column: "BookId",
+                principalTable: "Books",
+                principalColumn: "BookId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_BarrowBook_Users_UserId",
+                table: "BarrowBook",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Books_BookCategory_CategoryId",
+                table: "Books",
+                column: "CategoryId",
+                principalTable: "BookCategory",
+                principalColumn: "BCategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
