938c0eb639be7cd8f5eb0c12e985f23d88129473
maureenrubin <maureenrubin21@gmail.com>
2025-01-28 22:29:42 +0800

    Added Role Entity

---
+++ Domain/Entities/RoleEntity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Entities
+{
+    public class RoleEntity
+    {
+        [Key]
+        public int RoleId { get; set; }
+
+        public string? RoleName { get; set; }
+
+        public ICollection<UserRoleEntity> UserRoles { get; set; }
+    }
+}
---
+++ Domain/Entities/UserRoleEntity.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryManagementSystem.Domain.Entities
+{
+    public class UserRoleEntity
+    {
+        public int UserRoleId { get; set; }
+
+        public int UserId { get; set; }
+        public UserEntity User { get; set; }
+
+        public int RoleId { get; set; }
+        public RoleEntity Role { get; set; }
+
+    }
+}
