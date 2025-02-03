6a881e5d379c012bb6d26767faf11bb2643b5b0e
maureenrubin <maureenrubin21@gmail.com>
2025-02-03 23:04:47 +0800

    Modify Borrow Entity

--- Domain/Entities/BorrowTransaction.cs
+++ Domain/Entities/BorrowTransaction.cs
@@ -1,30 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LibraryManagementSystem.Domain.Entities
 {
-    public class BorrowBookEntity
+    public class BorrowTransaction
     {
-        [Key]
-        public int BarrowedItemId { get; set; }
-        public DateTime BarrowedDate { get; set; }
-        public DateTime? DueDate { get; set; }
 
-        public int? BookId { get; set; }
+        [Key]
+        public int BorrowedBookId { get; set; }
+        public DateTime BorrowedDate { get; set; }
+        public DateTime? DueDate { get; set; }
+        [Column(TypeName = "decimal(18,2)")]
+        public decimal BorrowedPrice { get; set; }
+        [Range(0, int.MaxValue)]
+        public int Quantity { get; set; }
+
+
+        public int BookId { get; set; }
+
+       
         public int UserId { get; set; }
 
 
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal BarrowedPrice { get; set; }
-        [Range(0, int.MaxValue)]
-        public int Quantity { get; set; }
-        public int BookStock  { get; set; }
-
+      
         public UserEntity User { get; set; }
         public BooksEntity Book { get; set; }
     }
